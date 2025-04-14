da3c7e34f8478044f741e55340696dfd84cb7b8b
diego-marques-miranda <diegomm.pu@gmail.com>
2025-04-14 13:25:06 -0300

    Update CityInput.jsx

--- Climatic/src/components/CityInput.jsx
+++ Climatic/src/components/CityInput.jsx
@@ -50,7 +50,7 @@ function CityInput()
                     <input onChange={(e) => handleInputChange(e)} value={inputCity} className="focus:outline-none w-[80%]" type="text" placeholder="Enter a city..."/>
                 </div>
 
-                <div className="bg-bg-2 p-[6px] rounded-lg">
+                <div className="bg-slate-800 p-[6px] rounded-lg">
                     <svg onClick={() => updateCity(inputCity)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-text-color">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
